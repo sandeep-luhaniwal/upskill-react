5c6c238cf2d959483df2d2f3d0e9ee2a57bc43c0
sandeep-luhaniwal <sandeepluh0001@gmail.com>
2024-09-25 11:12:41 +0530

    link and vercel done dom

--- src/components/pages/GithubLink.jsx
+++ src/components/pages/GithubLink.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const GithubLink = () => {
     return (
         <div className='min-h-[90vh] bg-black flex justify-center items-center border-b border-white/40'>
-            <Link to="" className='text-2xl hover:text-red duration-300 text-white'>Github Link is here</Link>
+            <Link to="https://github.com/sandeep-luhaniwal/upskill-react/tree/dom" target='_blank' className='text-2xl hover:text-red duration-300 text-white'>Github Link is here</Link>
         </div>
     )
 }
